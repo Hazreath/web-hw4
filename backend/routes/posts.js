7b5b900bb2e5499a44a1f460aef3e723f4b6b353
Hagotex <liivamario@gmail.com>
2020-12-08 17:37:47 +0200

    Task

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -23,21 +23,45 @@ router.get('/', authorize, (request, response) => {
 });
 
 router.post('/', authorize,  (request, response) => {
-
+    const params = {
+        userId: request.currentUser.id,
+        text: request.body.text,
+        media: {
+            type: request.body.media.type,
+            url: request.body.media.url,
+        },
+    }
+    /**
+     * Task#1 - 5 points
+     * https://sites.google.com/view/wad20-hw4/#h.qzvyq0stf1wp
+     */
     // Endpoint to create a new post
-
+    PostModel.create(params, (post) => {
+        return response.status(201).json(post)
+    })
 });
 
 
 router.put('/:postId/likes', authorize, (request, response) => {
-
+    /**
+     * Task#2 - 3 points
+     * https://sites.google.com/view/wad20-hw4/#h.q1b61xoqi58l
+     */
     // Endpoint for current user to like a post
+    PostModel.like(request.currentUser.id, request.params.postId, (r) => {
+        return response.status(201).json(r)
+    })
 });
 
 router.delete('/:postId/likes', authorize, (request, response) => {
-
+     /**
+     * Task#3 - 2 points
+     * https://sites.google.com/view/wad20-hw4/#h.n695yevfo1uz
+     */
     // Endpoint for current user to unlike a post
-
+    PostModel.unlike(request.currentUser.id, request.params.postId, (r) => {
+        return response.status(201).json(r)
+    })
 });
 
 module.exports = router;
